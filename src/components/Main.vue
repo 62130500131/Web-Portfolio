86543f467ca8f490b652f15816fab1a900ecc0cd
weeraphon <60165647+62130500131@users.noreply.github.com>
2021-10-29 01:12:40 +0700

    Add component

---
+++ src/components/Main.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="main" class="place-self-center grid grid-cols-2 py-52">
+
+    <img alt="photo of me" class="w-96 h-96 justify-self-center" src="../assets/Logo.png">
+
+    <div class="justify-self-start self-center text-left ">
+        <p >Hello , my name is </p>
+        <b class="filter brightness-200 ">Weeraphon Supachok.</b><br>
+        <b class="filter brightness-100 ">i build things for the website</b>
+    </div>
+
+  </div>
+
+  <div id="about" class="place-self-center grid grid-cols-2 py-52">
+
+    <img alt="photo of me" class="w-96 h-96 justify-self-center" src="../assets/Logo.png">
+
+    <div class="justify-self-start self-center text-left ">
+        <p >Hello , my name is </p>
+        <b class="filter brightness-200 ">Weeraphon Supachok.</b><br>
+        <b class="filter brightness-100 ">i build things for the website</b>
+    </div>
+
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+<style>
+    p{
+        font-size: 1.5rem;
+        line-height: 1.75rem;
+    }
+    b{
+        font-size: 3rem;
+        line-height: 1.6;
+    }
+</style>
+
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="grid grid-cols-2">
+        <div>
+
+        </div>
+        <div class="grid grid-cols-4 justify-self-end item-center">
+            <a  href="#main">About</a>
+            <a  href="#about">Education</a>
+            <a  href="#main">Skill</a>
+            <a  href="#main" class="filter brightness-200">Contract</a>
+        </div>
+    </div>
+</template>
+<style>
+
+a{
+    margin: 20px;
+    font-weight: 600;
+    font-size: 1rem;
+    line-height: 1rem;
+}
+</style>
