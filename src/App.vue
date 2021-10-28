54d7c5701d79db616f8a9e41a3a8a0f7f43aef5a
weeraphon <60165647+62130500131@users.noreply.github.com>
2021-10-29 01:12:04 +0700

    first version

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div >
+  <navbar/>
+  <Main/>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  
+}
+</script>
+
+<style>
+#app {
+  background-color: #0c1526;
+  height: 100%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #707d90;
+}
+</style>
